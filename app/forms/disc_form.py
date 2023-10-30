a374c30cf8ac6b24cc017302c305909a2d58b040
ian <iandkaneshiro@gmail.com>
2023-10-30 15:36:07 -0700

    fixed disc forms allowing 0 when applicable

--- app/forms/disc_form.py
+++ app/forms/disc_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, IntegerField, FloatField, BooleanField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms.validators import DataRequired, ValidationError, NumberRange
 from flask_wtf.file import FileField, FileAllowed
 from app.api.aws import ALLOWED_EXTENSIONS
 import re
@@ -13,10 +13,10 @@ class DiscForm(FlaskForm):
     type = StringField('type', validators=[DataRequired()])
     purchase_link = StringField('purchase_link')
     plastics = StringField('plastics', validators=[DataRequired()])
-    speed = IntegerField('speed', validators=[DataRequired()])
-    glide = IntegerField('glide', validators=[DataRequired()])
-    turn = FloatField('turn')
-    fade = FloatField('fade', validators=[DataRequired()])
+    speed = IntegerField('speed', validators=[NumberRange(1, 15)])
+    glide = IntegerField('glide', validators=[NumberRange(1, 7)])
+    turn = FloatField('turn', validators=[NumberRange(-5, 2)])
+    fade = FloatField('fade', validators=[NumberRange(0, 6)])
     diameter = FloatField('diameter')
     height = FloatField('height')
     rim_depth = FloatField('rim_depth')
